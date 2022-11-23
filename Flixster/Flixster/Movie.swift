c302808b565e990a38fea13b12643c3b79ee4a3f
Mari Batilando <mari.batilando@gmail.com>
2022-11-23 12:33:05 -0800

    [iOS 102][Unit 2] Add more mocks

--- Flixster/Flixster/Movie.swift
+++ Flixster/Flixster/Movie.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
+struct Movie: Codable {
+  let title: String
+  let description: String
+  let posterPath: String
+  let backdropPath: String
+  let voteAverage: Double
+  let voteCount: Int
+  let popularity: Double
+  
+  var posterURL: URL {
+    return URL(string: "https://image.tmdb.org/t/p/w185/\(posterPath)")!
+  }
+  var backdropURL: URL {
+    return URL(string: "https://image.tmdb.org/t/p/w1280\(backdropPath)")!
+  }
+}
+
 let HARDCODED_MOVIES = [Movie(title: "Black Adam",
                               description: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
                               posterPath: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
@@ -34,21 +51,39 @@ let HARDCODED_MOVIES = [Movie(title: "Black Adam",
                               backdropPath: "/omvnhpsp7nmW2HUgmrPrFcmUWIk.jpg",
                               voteAverage: 7.5,
                               voteCount: 165,
+                              popularity: 34.8),
+                        Movie(title: "Batman Beyond: Return of the Joker",
+                              description: "The Joker is back with a vengeance, and Gotham's newest Dark Knight, Terry McGinnis, needs answers as he stands alone to face Gotham's most infamous Clown Prince of Crime.",
+                              posterPath: "/7RlBs0An83fqAuKfwH5gKMcqgMc.jpg",
+                              backdropPath: "/aTlq9POtvgdheYGb5itxp32qg4S.jpg",
+                              voteAverage: 7.5,
+                              voteCount: 165,
+                              popularity: 34.8),
+                        Movie(title: "Creepshow",
+                              description: "Inspired by the E.C. comics of the 1950s, George A. Romero and Stephen King bring five tales of terror to the screen.",
+                              posterPath: "/4SoyTCEpsgLjX6yAyMsx3AsAyRQ.jpg",
+                              backdropPath: "/8aqTmCHLkktH7RZNALeaOMneHSl.jpg",
+                              voteAverage: 7.5,
+                              voteCount: 165,
+                              popularity: 34.8),
+                        Movie(title: "Astro Boy",
+                              description: "Set in the futuristic Metro City, Astro Boy (Atom) is a young robot with incredible powers created by a brilliant scientist in the image of the son he had lost. Unable to fulfill his creator's expectations, Astro embarks on a journey in search of acceptance, experiencing betrayal and a netherworld of robot gladiators, before returning to save Metro City and reconcile with the father who rejected him.",
+                              posterPath: "/fYOJaaCpqq1NatziVJntmsXXDi8.jpg",
+                              backdropPath: "/qXD5vfFe1blOXJbOHXlU5CYJMQB.jpg",
+                              voteAverage: 7.5,
+                              voteCount: 165,
+                              popularity: 34.8),
+                        Movie(title: "Mindhunters",
+                              description: "Trainees in the FBI's psychological profiling program must put their training into practice when they discover a killer in their midst.",
+                              posterPath: "/uTTGRvnqCI9ZC7WkyP9u7XRiOaA.jpg",
+                              backdropPath: "/j30knUpNbspOSehJi5SOJ4wvOVh.jpg",
+                              voteAverage: 7.5,
+                              voteCount: 165,
+                              popularity: 34.8),
+                        Movie(title: "Punisher: War Zone",
+                              description: "Waging his one-man war on the world of organized crime, ruthless vigilante-hero Frank Castle sets his sights on overeager mob boss Billy Russoti. After Russoti is left horribly disfigured by Castle, he sets out for vengeance under his new alias: Jigsaw. With the \"Punisher Task Force\" hot on his trail and the FBI unable to take Jigsaw in, Frank must stand up to the formidable army that Jigsaw has recruited before more of his evil deeds go unpunished.",
+                              posterPath: "/oOvKJgYUIpfswGHAdW6159bPbvM.jpg",
+                              backdropPath: "/dqPZpQy6PJINOJtld17NgIERusH.jpg",
+                              voteAverage: 7.5,
+                              voteCount: 165,
                               popularity: 34.8)]
-
-struct Movie: Codable {
-  let title: String
-  let description: String
-  let posterPath: String
-  let backdropPath: String
-  let voteAverage: Double
-  let voteCount: Int
-  let popularity: Double
-  
-  var posterURL: URL {
-    return URL(string: "https://image.tmdb.org/t/p/w185/\(posterPath)")!
-  }
-  var backdropURL: URL {
-    return URL(string: "https://image.tmdb.org/t/p/w1280\(backdropPath)")!
-  }
-}
